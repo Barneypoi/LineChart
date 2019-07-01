dde15df3a346c4003d2fc650cd82fc52954a7fba
Barneypoi <beiren.hi@163.com>
2019-07-01 17:24:03 +0800

    加入mark，删除description

--- app/src/main/java/com/example/test/ShowActivity.java
+++ app/src/main/java/com/example/test/ShowActivity.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.github.mikephil.charting.charts.LineChart;
+import com.github.mikephil.charting.components.Description;
 import com.github.mikephil.charting.components.XAxis;
 import com.github.mikephil.charting.data.Entry;
 import com.github.mikephil.charting.data.LineData;
@@ -16,7 +17,6 @@ import java.util.List;
 import java.util.Map;
 
 public class ShowActivity extends AppCompatActivity {
-
     public ArrayList<Map<String, Object>> mainlist=new ArrayList<Map<String,Object>>();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,22 +28,28 @@ public class ShowActivity extends AppCompatActivity {
         mLineChart.setDrawBorders(true);
         //设置数据
         List<Entry> entries = new ArrayList<>();
-        for (int i = 0; i < 4251; i++) {
+        for (int i = 0; i < mainlist.size(); i++) {
             entries.add(new Entry(i,Float.parseFloat((String) mainlist.get(i).get("po")) ) );
         }
         //准备好每个点对应的x轴数值
         List<String> list = new ArrayList<>();
-        for (int i = 0; i < 4251; i++) {
+        for (int i = 0; i < mainlist.size(); i++) {
             list.add(TimeStamp2Date((String) mainlist.get(i).get("1")));
         }
         XAxis xAxis = mLineChart.getXAxis();
         xAxis.setValueFormatter(new IndexAxisValueFormatter(list));
-
         //一个LineDataSet就是一条线
         LineDataSet lineDataSet;
         lineDataSet = new LineDataSet(entries, "净值");
         LineData data = new LineData(lineDataSet);
         mLineChart.setData(data);
+        DetailsMarkerView detailsMarkerView = new DetailsMarkerView(this,R.layout.detail);
+//一定要设置这个玩意，不然到点击到最边缘的时候不会自动调整布局
+        detailsMarkerView.setChartView(mLineChart);
+        mLineChart.setMarker(detailsMarkerView);
+        Description desc= new Description();
+        desc.setText("");
+        mLineChart.setDescription(desc);
 
     }
     public String TimeStamp2Date(String timestampString){
